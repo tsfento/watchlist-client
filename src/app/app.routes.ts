674c7f27be9a9ff47b87ad6bbb72a0753fc4a7b2
Tyler Fenton <tsfento@gmail.com>
2024-02-21 15:20:27 -0600

    Started lists component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./features/lists/lists.component').then((c) => c.ListsComponent),
+    // canActivate: [authGuard]
+  },
+];
