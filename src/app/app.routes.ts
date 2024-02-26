f75752e7a1a2a2b36f2d5b24c4b3be6c05b82d1f
Tyler Fenton <tsfento@gmail.com>
2024-02-26 13:50:35 -0600

    Added a home component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./features/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
     path: 'login',
     // pathMatch: 'full',
     loadComponent: () => import('./features/auth/login/login.component').then((c) => c.LoginComponent),
