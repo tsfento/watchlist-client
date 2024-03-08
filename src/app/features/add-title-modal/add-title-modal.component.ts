5135bc44f290dc92777276229a0f1376a878727a
Tyler Fenton <tsfento@gmail.com>
2024-03-08 12:04:54 -0600

    Setting up bootstrap modal for adding titles

--- src/app/features/add-title-modal/add-title-modal.component.ts
+++ src/app/features/add-title-modal/add-title-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+
+declare var window:any;
 
 @Component({
   selector: 'app-add-title-modal',
@@ -8,28 +10,33 @@ import { Component } from '@angular/core';
   styleUrl: './add-title-modal.component.scss'
 })
 export class AddTitleModalComponent {
-  openAddTitleModal() {
-    const modal:HTMLDialogElement | null = document.querySelector('.add-title-modal');
+  showAddTitleModal() {
 
-    modal!.showModal();
   }
 
-  closeAddTitleModal() {
-    const modal:HTMLDialogElement | null = document.querySelector('.add-title-modal');
+  // for non-bootstrap
+  // openAddTitleModal() {
+  //   const modal:HTMLDialogElement | null = document.querySelector('.add-title-modal');
 
-    modal!.close();
-  }
+  //   modal!.showModal();
+  // }
 
-  outsideDialog(e:MouseEvent) {
-    const dialog:HTMLDialogElement | null = document.querySelector('.add-title-modal');
-    const dialogDimensions = dialog!.getBoundingClientRect()
-  if (
-    e.clientX < dialogDimensions.left ||
-    e.clientX > dialogDimensions.right ||
-    e.clientY < dialogDimensions.top ||
-    e.clientY > dialogDimensions.bottom
-  ) {
-    dialog!.close()
-  }
-  }
+  // closeAddTitleModal() {
+  //   const modal:HTMLDialogElement | null = document.querySelector('.add-title-modal');
+
+  //   modal!.close();
+  // }
+
+  // outsideDialog(e:MouseEvent) {
+  //   const dialog:HTMLDialogElement | null = document.querySelector('.add-title-modal');
+  //   const dialogDimensions = dialog!.getBoundingClientRect()
+  //   if (
+  //     e.clientX < dialogDimensions.left ||
+  //     e.clientX > dialogDimensions.right ||
+  //     e.clientY < dialogDimensions.top ||
+  //     e.clientY > dialogDimensions.bottom
+  //   ) {
+  //     dialog!.close()
+  //   }
+  // }
 }
