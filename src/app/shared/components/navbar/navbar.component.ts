ddd369d99da10b2502c0bc86206886422e12f665
Tyler Fenton <tsfento@gmail.com>
2024-04-05 14:19:46 -0500

    Mobile navbar issues are due to Firefox 'scroll to hide toolbar'

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -36,8 +36,8 @@ export class NavbarComponent implements OnInit, OnDestroy {
   }
 
   isMobile() {
-    // return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    return true;
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    // return true;
   }
 
   isLoggedIn() {
