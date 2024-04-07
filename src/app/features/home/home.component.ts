b219b76f3a9115b75615e8f7c7a2a257868a867d
Tyler Fenton <tsfento@gmail.com>
2024-04-07 12:02:57 -0500

    Forgot to re-enable isMobile()

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -139,9 +139,9 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   isMobile() {
-    // return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     // return true;
-    return false;
+    // return false;
   }
 
   shuffle(array:UserWatchTitle[]) {
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -36,9 +36,9 @@ export class NavbarComponent implements OnInit, OnDestroy {
   }
 
   isMobile() {
-    // return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     // return true;
-    return false;
+    // return false;
   }
 
   isLoggedIn() {
