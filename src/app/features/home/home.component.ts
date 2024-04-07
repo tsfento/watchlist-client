335c9a83d398d61ffc01f9e408290aa48b1f42d7
Tyler Fenton <tsfento@gmail.com>
2024-04-07 12:24:23 -0500

    Working on active using indicators on home component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -174,13 +174,26 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
   }
 
-  setDefaultActive(drawerId:number, index:number) {
-    // TODO scrollIntoView
+  setDefaultActive(drawer:string, index:number) {
+    // TODO fix scrollIntoView
+    const drawerChildren = document.getElementById(drawer)?.children;
+
+    console.log(drawerChildren);
+
+    if (drawerChildren) {
+      for (let i = 0; i < drawerChildren?.length; i++) {
+        drawerChildren[i].children[0].classList.remove('active');
+      }
+      drawerChildren[index].children[0].classList.add('active');
+      drawerChildren[index].children[0].scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'end' });
+    }
   }
 
-  setRecsActive(drawerId:number, index:number) {
+  setRecsActive(drawer:string, index:number, event?:any) {
     // TODO fix scrollIntoView
-    const drawerChildren = document.getElementById("recsDrawer" + drawerId)?.children;
+    const drawerChildren = document.getElementById(drawer)?.children;
+
+    console.log(drawerChildren);
 
     if (drawerChildren) {
       for (let i = 0; i < drawerChildren?.length; i++) {
