2ef9d7c4fcd8b5604df863170f50793ef9e53526
Tyler Fenton <tsfento@gmail.com>
2024-04-10 16:24:07 -0500

    Working on adding an about-modal

---
+++ src/app/features/about-modal/about-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-modal.component.html',
+  styleUrl: './about-modal.component.scss'
+})
+export class AboutModalComponent {
+
+}
